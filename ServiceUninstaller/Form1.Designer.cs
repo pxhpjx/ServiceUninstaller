a1d68c63910d3186d20ce6981760815a6eac81ef
pxhpjx <pxhpjx@163.com>
2014-03-14 09:41:46 +0800

    Create

---
+++ ServiceUninstaller/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ServiceUninstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnUn = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(25, 12);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(185, 21);
+            this.txtName.TabIndex = 0;
+            // 
+            // btnUn
+            // 
+            this.btnUn.Location = new System.Drawing.Point(40, 45);
+            this.btnUn.Name = "btnUn";
+            this.btnUn.Size = new System.Drawing.Size(75, 23);
+            this.btnUn.TabIndex = 1;
+            this.btnUn.Text = "Uninstall";
+            this.btnUn.UseVisualStyleBackColor = true;
+            this.btnUn.Click += new System.EventHandler(this.btnUn_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(152, 50);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(35, 12);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "Ready";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(238, 87);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnUn);
+            this.Controls.Add(this.txtName);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ServiceUninstaller";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnUn;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ ServiceUninstaller/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ServiceUninstaller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUn_Click(object sender, EventArgs e)
+        {
+            WindowsServiceInstaller srv = new WindowsServiceInstaller();
+            if (txtName.Text != "")
+            {
+                if (srv.UnInstallService(txtName.Text))
+                    lblResult.Text = "OK";
+                else
+                    lblResult.Text = "Fail";
+            }
+            else
+                lblResult.Text = "Null";
+        }
+
+    }
+}
